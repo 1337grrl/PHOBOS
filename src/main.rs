8df1dd2bf13c3401d4b255700f873fcc37a704dc
deena_d <silja.rheingans@gmail.com>
2020-07-22 11:02:33 +0200

    in Cargo.toml: Cancelled stack unwinding by aborting when a panic happens. In main.rs: Overwriting main as entry point, thereby disabling crt0; disabling mangling for the _start function; using Ccalling convention for _start.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 // Disabling the standard library
 #![no_std]
+// Overwriting the usual entry point, since we can use neither Rust runtime nor C runtime Zero (crt0)
+// No runtime also means no main function
+#![no_main]
 
-fn main() {
-    println!("Hello, world!");
+use core::panic::PanicInfo;
+
+// Panic handler function
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+	loop {}
+}
+
+// Disabling name mangling 
+#[no_mangle]
+// By extern "C" we specify that _start is used by the calling convention of C, 
+//meaning it uses C's scheme for how subroutines receive parameters 
+//from their caller and how they return a result
+pub extern "C" fn _start() -> ! {
+	loop {}
 }
